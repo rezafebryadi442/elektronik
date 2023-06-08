b16e342e2d8e146757e5146fded404e1d6dfe7e2
rezafebryadi731 <135938043+rezafebryadi731@users.noreply.github.com>
2023-06-07 21:11:52 -0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,16 @@
+<?php
+
+$name=$_POST['name'];
+$email=$_POST['email'];
+$phone=$_POST['phone'];
+$message=$_POST['message'];
+
+
+echo "name: ".$name. "<br>";
+echo "email: ".$email. "<br>";
+echo "phone: ".$phone. "<br>";
+echo "message: ".$message. "<br>";
+
+
+?>
+<button><a href="index.html">Back</a></button>
